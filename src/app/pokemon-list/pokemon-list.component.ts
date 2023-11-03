a0bb4d8265500c6b351e51e4fcb26d52269ca478
JorgeReina <reinaromerojorge@gmail.com>
2023-11-03 17:29:33 +0100

    List component

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,1 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonBasicService } from '../pokemonBasic.service';
 
+
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent implements OnInit{
+
+  data: any = "";
+
+  constructor(private pokemonBasicService: PokemonBasicService) {
+
+  }
+  ngOnInit(): void {
+    this.llenarData()
+  }
+
+  llenarData() {
+    this.pokemonBasicService.getInfo().subscribe( data => {
+      this.data = data;
+      console.log(this.data)
+    })
+  }
+
+}
