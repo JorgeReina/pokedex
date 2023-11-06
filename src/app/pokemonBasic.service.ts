b0cc0462ab62aef260054f30906ebb38c73c32c5
JorgeReina <reinaromerojorge@gmail.com>
2023-11-06 16:24:26 +0100

    Lista Pokemon 50%

--- src/app/pokemonBasic.service.ts
+++ src/app/pokemonBasic.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, forkJoin, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +9,17 @@ export class PokemonBasicService {
 
   constructor(private http: HttpClient) { }
 
-  public getInfo2() {
-    console.log(this.http.get<any[]>(`https://pokeapi.co/api/v2/pokemon/`));
+  public getInfo(): Observable<any[]> {
+
+    let pokemons: Observable<any>[] = [];
+
+    for (let i = 1; i < 151; i++) {
+      pokemons.push(this.http.get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+      .pipe(map((response: any) => response)));
+    }
+
+    return forkJoin(pokemons);
+
   }
 
-  public getInfo(): Observable<any> {
-
-    //Pokemon[]
-    console.log(this.http.get<any[]>(`https://pokeapi.co/api/v2/pokemon/3`));
-
-    return this.http.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=151`)
-      .pipe(map((response: any) => response.results)); /*?offset=20&limit=151 */
-  }
 }
