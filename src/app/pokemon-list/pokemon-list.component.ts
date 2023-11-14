95db8e74ff6a65edf7457bd89b8e57b5b813f2c9
Angelinero48 <116581341+Angelinero48@users.noreply.github.com>
2023-11-14 19:19:51 +0100

    Filtro tipos y buscador

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -28,17 +28,31 @@ export class PokemonListComponent implements OnInit{
   }
   @Input()
   filter: any;
-  types: any;
+  types: any[] = [];
 
   getData(): Pokemon[]{
     return this.data;
   }
+  getTypeFiltered(type: any){
+    this.types.push(type)
+  }
+  getDataFiltered(): Pokemon[] {
+    return this.data.filter((pokemon) => {
+      const nameMatch = pokemon.name.includes(this.filter) || pokemon.id.toString().includes(this.filter);
+      const typeMatch = this.types && this.types.length == 0 || this.types.some((type: string) => pokemon.types.includes(type));
+      return nameMatch && typeMatch;
+    });
+  }
+  resetFilters(){
+    this.filter = ""
+    this.types = []
+  }
+  /*
     getDataFiltered(): Pokemon[]{
     return this.getData().filter((pokemons)=>pokemons.name.includes(this.filter) || pokemons.id.toString().includes(this.filter));
-  }
+  }*/
 
-  getTypeFiltered(types: any): Pokemon[]{
-    this.types = types
-    return this.getData().filter((pokemons) => pokemons.types.includes(this.types));
-  }
+
+
 }
+
