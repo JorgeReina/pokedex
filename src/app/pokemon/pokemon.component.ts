9e83ecc3a4e01ccfb2e9c45e714dd6730cdd0b67
Jorge Reina <reinaromerojorge@gmail.com>
2023-11-22 12:56:49 +0100

    Tabla de tipos con estilos y last comprobaciones

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -4,7 +4,7 @@ import { PokemonInfo } from '../interface/pokemonInfo';
 import { ActivatedRoute } from "@angular/router";
 import { Description } from '../interface/description';
 import { Damages } from '../interface/damages';
-import { Observable, delay, forkJoin, map, pipe, tap } from 'rxjs';
+import { Observable, delay, forkJoin, map, of, pipe, tap } from 'rxjs';
 import { TableDamages } from '../interface/tableDamages';
 
 @Component({
@@ -63,25 +63,59 @@ export class PokemonComponent implements OnInit{
     this.route.params.subscribe(params => {
       this.numberList = params['id'];
 
-      forkJoin([
-        this.setData(),
-        this.setDescription()
-      ]).pipe(
-        // Agrega un delay de 300 milisegundos (0.3 segundo)
-        delay(300),
-        tap(() => {
-          if (this.infoPokemon.types.length > 0) {
-            this.showTableDamage(0, this.type1, 1);
-            this.showTableDamage(1, this.type2, 2);
-          } else {
-            this.showTableDamage(0, this.type1, 1);
-          }
-        })
-      ).subscribe(() => {
-        // Otras operaciones después del retraso
-      });
-
     });
+
+    forkJoin([
+      this.setData(),
+      this.setDescription()
+    ]).pipe(
+      delay(200),
+      tap(() => {
+
+        if (this.infoPokemon.types.length > 1) {
+          this.pokemonBasicService.getIdTypes(this.infoPokemon.types[0]).subscribe(
+            (id: number) => {
+              this.type1 = id;
+              this.setTableDamage(this.type1, 1).subscribe(
+                (tableDamages: any) => {
+                  this.tableDamages = tableDamages as Damages;
+                  console.log(this.tableDamages);
+                }
+              );
+            }
+          );
+
+          this.pokemonBasicService.getIdTypes(this.infoPokemon.types[1]).subscribe(
+            (id: number) => {
+              this.type2 = id;
+              this.setTableDamage(this.type2, 2).subscribe(
+                (tableDamages: any) => {
+                  this.tableDamages2 = tableDamages as Damages;
+                  console.log(this.tableDamages2);
+                  this.joinTableDamage()
+                  console.log(this.tableDamagesFull)
+                }
+              );
+            }
+          )
+        } else {
+          this.pokemonBasicService.getIdTypes(this.infoPokemon.types[0]).subscribe(
+            (id: number) => {
+              this.type1 = id;
+              this.setTableDamage(this.type1, 1).subscribe(
+                (tableDamages: any) => {
+                  this.tableDamages = tableDamages as Damages;
+                  console.log(this.tableDamages);
+                  this.joinTableDamage()
+                  console.log(this.tableDamagesFull)
+                });
+            });
+        }
+      })
+    ).subscribe(() => {
+      // Otras operaciones después del retraso
+    });
+    
   }
       
   //  FUNCION PARA OBTENER LOS DATOS DESDE LA API Y GUARDARLOS EN DATA
@@ -98,6 +132,18 @@ export class PokemonComponent implements OnInit{
     );
   }
 
+  //  FUNCION PARA OBTENER LA TABLA DE DAÑOS
+  setTableDamage(types: number, option: number): Observable<Damages> {
+    switch (option) {
+      case 1:
+        return this.pokemonBasicService.getTableDamage(types);
+      case 2:
+        return this.pokemonBasicService.getTableDamage(types);
+      default:
+        return of(); // Puedes ajustar esto según tus necesidades
+    }
+  }
+
   //  METODO QUE DEVUELVE CORRECTAMENTE LA DESCRIPCION DEL POKEMON CON LOS SALTOS DE LINEA
   formatDescription(description: string): string {
     const formattedText = description.replace(/\n/g, '<br>');
@@ -106,39 +152,41 @@ export class PokemonComponent implements OnInit{
 
   //  METDODO QUE UNIFICA LAS TABLAS
   joinTableDamage() {
-    this.tableDamagesFull.x4 = this.tableDamages.doubleDamageFrom && this.tableDamages2.doubleDamageFrom
-  }
 
-  //  METODO QUE MUESTRA LAS TABLAS RELLENAS
-  showTableDamage(n: number, type:number, option: number) {
-    this.pokemonBasicService.getIdTypes(this.infoPokemon.types[n]).subscribe(
-      (id: number) => {
-        type = id;
-        //console.log(type);
-        this.setTableDamage(type, option)
-      });
-  }
+    this.tableDamagesFull.doubleDamageFrom = Array.from(new Set([...this.tableDamages.doubleDamageFrom,...this.tableDamages2.doubleDamageFrom]))
 
-  //  METODO QUE DEVULEVE LA TABLA DE DAÑOS
-  setTableDamage(types: number, option: number) {
-    
-    switch (option) {
-      case 1:
-        this.pokemonBasicService.getTableDamage(types).subscribe((tableDamages) => {
-          this.tableDamages = tableDamages as Damages;
-          //console.log(this.tableDamages)
-        });
-        break;
-      case 2:
-        this.pokemonBasicService.getTableDamage(types).subscribe((tableDamages) => {
-          this.tableDamages2 = tableDamages as Damages;
-          //console.log(this.tableDamages2)
-        });
-        break;
-      default:
-        break;
-    }
-    
+    this.tableDamagesFull.halfDamageFrom = Array.from(new Set([...this.tableDamages.halfDamageFrom, ...this.tableDamages2.halfDamageFrom]));
+
+    this.tableDamagesFull.noDamageFrom = Array.from(new Set([...this.tableDamages.noDamageFrom,...this.tableDamages2.noDamageFrom]));
+
+    // Encontrar duplicados y agregar a los arrays correspondientes en tableDamagesFull
+    this.tableDamages.doubleDamageFrom.forEach((type) => {
+      if (this.tableDamages2.doubleDamageFrom.includes(type)) {
+        this.tableDamagesFull.x4.push(type);
+      }
+    });
+    // Encontrar duplicados y agregar a los arrays correspondientes en tableDamagesFull
+    this.tableDamages.halfDamageFrom.forEach((type) => {
+      if (this.tableDamages2.halfDamageFrom.includes(type)) {
+        this.tableDamagesFull.quarterDamageFrom.push(type);
+      }
+    });
+
+    // Eliminar tipos en doubleDamageFrom si ya están en x4
+    this.tableDamagesFull.doubleDamageFrom = this.tableDamagesFull.doubleDamageFrom.filter(
+      (type) => !this.tableDamagesFull.x4.includes(type)
+    );
+
+    // Eliminar tipos en halfDamageFrom si ya están en quarterDamageFrom
+    this.tableDamagesFull.halfDamageFrom = this.tableDamagesFull.halfDamageFrom.filter(
+      (type) => !this.tableDamagesFull.quarterDamageFrom.includes(type)
+    );
+
+    // Eliminar tipos en halfDamageFrom si ya están en quarterDamageFrom
+    this.tableDamagesFull.doubleDamageFrom = this.tableDamagesFull.doubleDamageFrom.filter(
+      (type) => !this.tableDamagesFull.noDamageFrom.includes(type)
+    );
+
   }
 
   //Cambiar a shiny
@@ -148,7 +196,7 @@ export class PokemonComponent implements OnInit{
     this.shinyMode = !this.shinyMode;
   }
 
-  //  
+  //  OBTINE LA IMAGEN DEL TIPO EN LA CARTA
   getImagePath(type: string): string {
     return `/assets/images/types/${type.toLowerCase()}.png`;
   }
