3fda2b276e83e29492aaa8f6019d0606b3c482a5
JorgeReina <reinaromerojorge@gmail.com>
2023-11-27 16:00:32 +0100

    Types

---
+++ src/app/evolution-chain/evolution-chain.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evolution-chain',
+  templateUrl: './evolution-chain.component.html',
+  styleUrls: ['./evolution-chain.component.css']
+})
+export class EvolutionChainComponent {
+
+}
